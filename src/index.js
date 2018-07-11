dafa45661a50d10500475ded135ddfc6593c6984
errorr404 <dkbishwash@gmail.com>
2018-07-11 13:48:24 +0530

    Setup the Project and addPost and CreatePost method

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+global.jQuery = require('jquery');
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
